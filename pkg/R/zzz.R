a8c469453867de03b52edf300befd8f965f0dc0c
ydesjeux <ydesjeux@d86d4d0f-2339-4a85-96f4-68458ae66a9b>
2021-02-22 12:32:26 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/sfar@5 d86d4d0f-2339-4a85-96f4-68458ae66a9b

--- pkg/R/zzz.R
+++
@@ -1,9 +0,0 @@
-# globalVariables(c("EffRes", "LogNorm"))
-
-# .onAttach <- function(lib, pkg) {
-    # packageStartupMessage(paste0("\n* Please cite the 'sfaR' package as:\n", 
-        # "  Dakpo K.H., Desjeux Y. and Latruffe L. (2021). sfaR: Stochastic Frontier Analysis using R. R package version 0.0.1.\n\n", 
-        # "See also: citation(\"sfaR\")\n\n", 
-        # "* For any questions, suggestions, or comments on the 'productivity' package, please make use of Tracker facilities at:\n",
-        # "  https://r-forge.r-project.org/projects/productivity/\n"))
-# }
